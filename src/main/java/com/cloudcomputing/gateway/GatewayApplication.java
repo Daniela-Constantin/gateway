ee33083603a6cbf30f01885ee3b2e7f9709ddef3
Daniela-Constantin <danielaconstantin90@yahoo.com>
2023-01-10 21:04:08 +0200

    Initial commit for gateway project

---
+++ src/main/java/com/cloudcomputing/gateway/GatewayApplication.java
@@ -0,0 +1,26 @@
+package com.cloudcomputing.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+	@Bean
+	RouteLocator gatewayRoute(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route("shoppingCartRoute", routeSepc -> routeSepc
+						.path("/shoppingcart/**")
+						.filters(f -> f.addResponseHeader("X-Response-Header", "SCS"))
+						.uri("lb://shoppingcart-service") //load balancer in case of multiple clients and routing to the eureka server
+				)
+				.build();
+	}
+}
